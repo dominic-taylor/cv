87dfdd6396f13eae05d7622fa4c97059643405ab
dominic-taylor <dataylorx@gmail.com>
2016-05-26 15:02:51 +1200

    Add arrow nav functionality

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 function changeContent() {
   var current = document.getElementsByClassName('content')
-  console.log(current)
 
-  // make current content DISPLAY NONE
-  // make next content DISPLAY
+  if( current[0].style.display == 'block') {
+    current[0].style.display = 'none'
+    current[1].style.display = 'block'
+  }
+  else if( current[1].style.display == 'block') {
+    current[1].style.display = 'none'
+    current[2].style.display = 'block'
+  }
+  else if( current[2].style.display == 'block') {
+    current[2].style.display = 'none'
+    current[3].style.display = 'block'
+  }
+  else  {
+    current[3].style.display = 'none'
+    current[0].style.display = 'block'
+  }
 }
 
 var arrow = document.getElementById('arrow')
