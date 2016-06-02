a89f57d0e5bf5e77255a35f084ba3e3ada0bd2ab
Maxine Ellah <dataylorx@gmail.com>
2016-06-02 12:39:40 +1200

    Add refactoring

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 function changeContent() { // refactor
   var current = document.getElementsByClassName('content')
-
-  if(current[0].style.display == 'block') {
-    current[0].style.display = 'none'
-    current[1].style.display = 'block'
+  for(var i = 0; i<current.length; i++){
+    if(current[i].style.display == 'block'){
+        i++
+      }
+    }
   }
-  else if( current[1].style.display == 'block') {
-    current[1].style.display = 'none'
-    current[2].style.display = 'block'
-  }
-  else if( current[2].style.display == 'block') {
-    current[2].style.display = 'none'
-    current[3].style.display = 'block'
-  }
-  else  {
-    current[3].style.display = 'none'
-    current[0].style.display = 'block'
-  }
+  // if(current[0].style.display == 'block') {
+  //   current[0].style.display = 'none'
+  //   current[1].style.display = 'block'
+  // }
+  // else if( current[1].style.display == 'block') {
+  //   current[1].style.display = 'none'
+  //   current[2].style.display = 'block'
+  // }
+  // else if( current[2].style.display == 'block') {
+  //   current[2].style.display = 'none'
+  //   current[3].style.display = 'block'
+  // }
+  // else  {
+  //   current[3].style.display = 'none'
+  //   current[0].style.display = 'block'
+  // }
 }
 
 function selectContent(){
