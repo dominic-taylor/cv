68cab74358151d71382e75882f8f2d3e5a8da9b8
Maxine Ellah <dataylorx@gmail.com>
2016-06-17 16:57:45 +1200

    Add extracted method refactor

--- main.js
+++ main.js
@@ -7,26 +7,48 @@
     menuItems[i].addEventListener('click', selectContent, false)
   }
 
+function checkContentVisible() {
+  var content = document.getElementsByClassName('content');
+  for (var i = 0; i < content.length; i++)   {
+    console.log('content.style ' + content[i].style.display);
+   if(content[i].style.display == 'block') {
+     console.log('hit checkContentVisible if loop');
 
-function nextContent()  { // refacto
-var content = document.getElementsByClassName('content')
-console.log(content);
-    if(content[0].style.display == 'block') {
-      content[0].style.display = 'none'
-      content[1].style.display = 'block'
-    }
-    else if( content[1].style.display == 'block') {
-      content[1].style.display = 'none'
-      content[2].style.display = 'block'
-    }
-    else if( content[2].style.display == 'block') {
-      content[2].style.display = 'none'
-      content[3].style.display = 'block'
-    }
-    else  {
-      content[3].style.display = 'none'
-      content[0].style.display = 'block'
-    }
+     console.log('v i ', i);
+     return i
+   }
+  }
+}
+
+function nextContent()  { // refactor with check what content
+  var content = document.getElementsByClassName('content')
+  var visible = checkContentVisible()
+  console.log('v ', visible);
+  if (visible == 3)  {
+    content[3].style.display = 'none'
+    content[0].style.display = 'block'
+  }
+  else {
+    content[visible].style.display = 'none'
+    content[visible+1].style.display = 'block'
+  }
+  // if(content[0].style.display == 'block') {
+  //     content[0].style.display = 'none'
+  //     content[1].style.display = 'block'
+  //   }
+  //   else if( content[1].style.display == 'block') {
+  //     console.log(' 2nd content block');
+  //     content[1].style.display = 'none'
+  //     content[2].style.display = 'block'
+  //   }
+  //   else if( content[2].style.display == 'block') {
+  //     content[2].style.display = 'none'
+  //     content[3].style.display = 'block'
+  //   }
+  //   else  {
+  //     content[3].style.display = 'none'
+  //     content[0].style.display = 'block'
+  //   }
 
   }
 
